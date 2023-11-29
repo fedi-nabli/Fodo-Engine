8fb0b65a334b61d25b72642d7a26e144be960652
fedi-nabli <fedinabli@gmail.com>
2023-11-29 20:27:22 +0100

    Added premake and generated projects

---
+++ premake5.lua
@@ -0,0 +1,109 @@
+workspace "Fodo"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	startproject "Sandbox"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Fodo"
+	location "Fodo"
+	kind "SharedLib"
+	language "C++"
+	toolset "v142"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"FD_PLATFORM_WINDOWS",
+			"FD_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "FD_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "FD_DEBUG"
+		optimize "On"
+	
+	filter "configurations:Dist"
+		defines "FD_DEBUG"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	toolset "v142"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Fodo/vendor/spdlog/include",
+		"Fodo/src"
+	}
+
+	links
+	{
+		"Fodo"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"FD_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "FD_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "FD_DEBUG"
+		optimize "On"
+	
+	filter "configurations:Dist"
+		defines "FD_DEBUG"
+		optimize "On"
