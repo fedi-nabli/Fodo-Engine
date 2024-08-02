ccb9d155ca85f0da1764331a7d9da501466dae0f
fedi-nabli <fedinabli@gmail.com>
2024-08-02 12:31:52 +0100

    Chnaged spdlog version

--- Fodo/src/Fodo/Application.cpp
+++ Fodo/src/Fodo/Application.cpp
@@ -20,21 +20,21 @@ namespace Fodo {
 		WindowResizeEvent e(1280, 720);
 		if (e.IsInCategory(EventCategoryApplication))
 		{
-			FD_TRACE("{}", e.ToString());
+			FD_TRACE(e);
 		}
 		if (e.IsInCategory(EventCategoryInput))
 		{
-			FD_TRACE("{}", e.ToString());
+			FD_TRACE(e);
 		}
 
 		MouseScrolledEvent e2(20, 30);
 		if (e2.IsInCategory(EventCategoryMouse))
 		{
-			FD_TRACE("{}", e2.ToString());
+			FD_TRACE(e2);
 		}
 		if (e2.IsInCategory(EventCategoryInput))
 		{
-			FD_TRACE("{}", e2.ToString());
+			FD_TRACE(e2);
 		}
 
 		while (true);
